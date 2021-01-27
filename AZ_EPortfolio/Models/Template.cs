4fb50232c130eb6120a8a966e8f4c6f3e469e842
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:53:16 +0000

    Added Template class and attributes

---
+++ AZ_EPortfolio/Models/Template.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class Template
+    {
+		private int templateID;
+		private String summary;
+		private blob picture;
+		private String skillTags;
+		private String resume;
+		private String education;
+	}
+}
