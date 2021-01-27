7247c1c01e0ac62df82c338319e1983130ee249e
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:43:49 +0000

    Added FileEvidence class and attributes

---
+++ AZ_EPortfolio/Models/FileEvidence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class FileEvidence : Evidence
+    {
+        private int fileEvidenceID;
+        private String fileName;
+        private FileFormats fileFormat;
+
+    }
+}
