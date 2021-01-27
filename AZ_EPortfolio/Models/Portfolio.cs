6e3554b3362afb79ca333e42cc0111ff0c22111d
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 14:50:03 +0000

    Added Annotations to Portfolio class

--- AZ_EPortfolio/Models/Portfolio.cs
+++ AZ_EPortfolio/Models/Portfolio.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,24 @@ namespace AZ_EPortfolio.Models
 {
     public class Portfolio
     {
+        [Key]
         private string PortfolioId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Skills")]
         private string Skills { get; set; }
+
+        [Required]
+        [StringLength(5000)]
+        [DisplayName("Knowledge")]
+        [DataType(DataType.MultilineText)]
         private string Knowledge { get; set; }
+
+        [Required]
+        [StringLength(5000)]
+        [DisplayName("Experience")]
+        [DataType(DataType.MultilineText)]
         private string Experience { get; set; }
 
     }
