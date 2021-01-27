2bc3b76a5cfa3e773a5b054083efe262602e1b10
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 16:16:55 +0000

    Added Annotations to Employer class

--- AZ_EPortfolio/Models/Employer.cs
+++ AZ_EPortfolio/Models/Employer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,18 @@ namespace AZ_EPortfolio.Models
 {
     public class Employer : User
     {
+        [Key]
         private string EmployerId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Company Name")]
         private string CompanyName { get; set; }
+
+        [Required]
+        [StringLength(5000)]
+        [DisplayName("Description")]
+        [DataType(DataType.MultilineText)]
         private string Description { get; set; }
     }
 }
