1ded265e50cc063898d72d205b8bcaf5faa386bd
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:48:29 +0000

    Added ImageEvidence class and attributes

---
+++ AZ_EPortfolio/Models/ImageEvidence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+        public class ImageEvidence : Evidence
+        {
+            private int imageEvidenceID;
+            private String imageName;
+            private ImageFormats imageFormat;
+            private double height;
+            private double width;
+
+        }
+}
