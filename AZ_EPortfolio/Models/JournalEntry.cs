1fa62848d8ce4b6d93abe605d2d430690df8e948
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 15:40:04 +0000

    Added Annotations to JournalEntry class

--- AZ_EPortfolio/Models/JournalEntry.cs
+++ AZ_EPortfolio/Models/JournalEntry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,18 @@ namespace AZ_EPortfolio.Models
 {
     public class JournalEntry
     {
+        [Key]
         private string JournalEntryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Title")]
         private string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         private DateTime date { get; set; }
 
     }
