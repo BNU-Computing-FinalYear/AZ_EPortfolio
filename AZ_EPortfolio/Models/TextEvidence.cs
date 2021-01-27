cab89aacb96a9ae249ec47eea525ee7cd0d7beb0
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:58:07 +0000

    Added TextEvidence class and attributes

---
+++ AZ_EPortfolio/Models/TextEvidence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class TextEvidence : Evidence
+    {
+        private int textEvidenceID;
+        private String text;
+        private TextFormats textFormat;
+
+    }
+}
