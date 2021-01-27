e1f063023951a67c651fccda404f23f9cff05feb
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:50:04 +0000

    Added JournalEntry class and attributes

---
+++ AZ_EPortfolio/Models/JournalEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class JournalEntry
+    {
+        private int journalEntryID;
+        private String title;
+        private date date;
+        private int imageEvidenceID;
+        private int textEvidenceID;
+
+    }
+}
