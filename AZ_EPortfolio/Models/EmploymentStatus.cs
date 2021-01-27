bed77a01c978de6d855a5ac6163cc90a4664985b
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:38:44 +0000

    Added EmploymentStatus class and attributes

---
+++ AZ_EPortfolio/Models/EmploymentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public enum EmploymentStatus
+    {
+        Fulltime,
+        Parttime,
+        Internship,
+        Unemployed,
+
+    }
+}
