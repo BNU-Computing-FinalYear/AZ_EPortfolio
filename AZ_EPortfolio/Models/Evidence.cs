cb881d58a5ef1fc6c255ed9cf77c654eb08ce0da
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 16:15:30 +0000

    Added Annotations to Evidence class

--- AZ_EPortfolio/Models/Evidence.cs
+++ AZ_EPortfolio/Models/Evidence.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,14 @@ namespace AZ_EPortfolio.Models
 {
     public class Evidence
     {
+        [Key]
         private string EvidenceId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DisplayName("Evidence Name")]
         private string EvidenceName { get; set; }
+
         private EvidenceTypes EvidenceType { get; set; }
     }
 }
