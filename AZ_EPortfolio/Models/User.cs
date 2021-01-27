73720858bc5b7e14a65911a279f17a61a1e580be
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 14:40:12 +0000

    Added Annotations to User class

--- AZ_EPortfolio/Models/User.cs
+++ AZ_EPortfolio/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,32 @@ namespace AZ_EPortfolio.Models
 {
     public class User
     {
+		[Key]
 		private string UserId { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DisplayName("First Name")]
 		private string FirstName { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DisplayName("Last Name")]
 		private string LastName { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[DisplayName("Career")]
 		private string Career { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DataType(DataType.EmailAddress)]
 		private string Email { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		[DataType(DataType.Password)]
 		private string Password { get; set; }
 
 	}
