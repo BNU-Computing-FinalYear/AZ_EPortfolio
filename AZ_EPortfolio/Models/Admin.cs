a6e6da4ce88fc1eedcceb381d7397889a80e734a
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 16:18:19 +0000

    Added Annotations to Admin class

--- AZ_EPortfolio/Models/Admin.cs
+++ AZ_EPortfolio/Models/Admin.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,13 @@ namespace AZ_EPortfolio.Models
 {
     public class Admin : User
     {
+		[Key]
 		private int AdminId { get; set; }
-		private string Role { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Role Name")]
+        private string Role { get; set; }
 
 	}
 }
