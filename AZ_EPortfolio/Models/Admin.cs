e17a4e28a160641cb2eafb5be7c9a1dc5854ad9a
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:36:36 +0000

    Added Admin class and attributes

---
+++ AZ_EPortfolio/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class Admin : User
+    {
+			private int adminID;
+			private String role;
+
+	}
+}
