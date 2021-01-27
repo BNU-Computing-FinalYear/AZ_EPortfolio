bcca7fe0359b020d73174e3f00b3a4e9b143e3b4
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:31:28 +0000

    Added User class and its attributes

---
+++ AZ_EPortfolio/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class User
+    {
+		private int userID;
+		private String firstName;
+		private String lastName;
+		private String career;
+		private String email;
+		private String password;
+
+	}
+}
