7edd8dc2c57bef40b424bc6034ca64e1ba9e22ed
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 14:06:50 +0000

    Added getters and setters

--- AZ_EPortfolio/Models/Employer.cs
+++ AZ_EPortfolio/Models/Employer.cs
@@ -7,8 +7,8 @@ namespace AZ_EPortfolio.Models
 {
     public class Employer : User
     {
-        private int employerID;
-        private String companyName;
-        private String description;
+        private string EmployerId { get; set }
+        private string CompanyName { get; set }
+        private string Description { get; set }
     }
 }
