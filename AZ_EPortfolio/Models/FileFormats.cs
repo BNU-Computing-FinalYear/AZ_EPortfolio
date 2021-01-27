ee8e3706442a640e2d8796340217473e1bf6052d
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:47:23 +0000

    Added FileFormats enumeration

---
+++ AZ_EPortfolio/Models/FileFormats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public enum FileFormats
+    {
+		DOCX,
+		XLS,
+		TXT,
+		PDF,
+		PPTX,
+		XML,
+		MP3,
+		WAV,
+		MP4,
+    }
+}
