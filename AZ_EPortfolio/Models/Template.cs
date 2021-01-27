7fb52b8a974753d82b68bc9af5b28eedf2ec1307
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 14:47:12 +0000

    Added Annotations to Template class

--- AZ_EPortfolio/Models/Template.cs
+++ AZ_EPortfolio/Models/Template.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,36 @@ namespace AZ_EPortfolio.Models
 {
     public class Template
     {
+		[Key]
 		private string TemplateId { get; set; }
+
+		[Required]
+		[StringLength(5000)]
+		[DisplayName("Description")]
+		[DataType(DataType.MultilineText)]
 		private string Summary { get; set; }
-		private blob Iicture { get; set; }
+
+		[Required]
+		[StringLength(500)]
+		[DataType(DataType.Url)]
+		[DisplayName("Image URL")]
+		private string Picture { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[DisplayName("Description")]
 		private string SkillTags { get; set; }
+
+		[Required]
+		[StringLength(5000)]
+		[DisplayName("Description")]
+		[DataType(DataType.MultilineText)]
 		private string Resume { get; set; }
+
+		[Required]
+		[StringLength(5000)]
+		[DisplayName("Description")]
+		[DataType(DataType.MultilineText)]
 		private string Education { get; set; }
 	}
 }
