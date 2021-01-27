f574ba663c181091ee2866d167af8963d9ca4750
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:40:35 +0000

    Added Evidence class and attributes

---
+++ AZ_EPortfolio/Models/EvidenceStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class Evidence
+    {
+        private int evidenceID;
+        private String evidenceName;
+        private EvidenceTypes evidenceType;
+    }
+}
