49aea9161332887e26d1b59af941a2b152da4fe8
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 15:38:28 +0000

    Added Annotations to Owner class

--- AZ_EPortfolio/Models/Owner.cs
+++ AZ_EPortfolio/Models/Owner.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,16 @@ namespace AZ_EPortfolio.Models
 {
     public class Owner : User
     {
+        [Key]
         private string OwnerId { get; set; }
+
         private EmploymentStatus EmploymentStatus { get; set; }
+
+        [Required]
+        [StringLength(16, ErrorMessage = "Please enter a valid UK contact number")]
+        [DisplayName("Mobile No")]
         private int MobileNo { get; set; }
+
         private OwnerTypes OwnerType { get; set; }
 
     }
