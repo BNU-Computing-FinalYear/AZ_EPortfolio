079dba59baa8973f49d1b4b589f4f3d7fb7523c2
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 16:13:06 +0000

    Added Annotations to FileEvidence class

--- AZ_EPortfolio/Models/FileEvidence.cs
+++ AZ_EPortfolio/Models/FileEvidence.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,14 @@ namespace AZ_EPortfolio.Models
 {
     public class FileEvidence : Evidence
     {
+        [Key]
         private string FileEvidenceId { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [DisplayName("File Name")]
         private string FileName { get; set; }
+
         private FileFormats FileFormat { get; set; }
 
     }
