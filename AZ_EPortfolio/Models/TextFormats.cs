2dd7d9aa1d77502c42263285a18b696ba7adeb92
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 14:01:15 +0000

    Added TextFormats enumeration

---
+++ AZ_EPortfolio/Models/TextFormats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public enum TextFormats
+    {
+		Paragraph,
+		OrderedList,
+		UnorderedList,
+		URL,
+		Font,
+		Styles,
+		Alignment,
+		Indentation,
+	}
+}
