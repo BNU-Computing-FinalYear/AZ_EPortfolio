2f7d4ee140f84db95c45c3d80a8bdb3789a55c28
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:52:31 +0000

    Added Portfolio class and attributes

---
+++ AZ_EPortfolio/Models/Portfolio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class Portfolio
+    {
+        private int portfolioID;
+        private String skills;
+        private String knowledge;
+        private String experience;
+
+    }
+}
