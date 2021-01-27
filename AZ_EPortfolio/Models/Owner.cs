adcb51679ddf7d4588d18e8b7a10f87cd3404663
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:50:58 +0000

    Added Owner class and attributes

---
+++ AZ_EPortfolio/Models/Owner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class Owner
+    {
+        private int ownerID;
+        private EmploymentStatus employmentStatus;
+        private int mobileNo;
+        private OwnerTypes ownerType;
+
+    }
+}
