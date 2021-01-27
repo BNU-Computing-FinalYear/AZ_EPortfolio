e996226f085724a32b7eb46f9031a898c16d046a
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 15:44:30 +0000

    Added Annotations to ImageEvidence class

--- AZ_EPortfolio/Models/ImageEvidence.cs
+++ AZ_EPortfolio/Models/ImageEvidence.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,18 @@ namespace AZ_EPortfolio.Models
 {
         public class ImageEvidence : Evidence
         {
+            [Key]
             private string ImageEvidenceId { get; set; }
+
+            [Required]
+            [StringLength(20)]
+            [DisplayName("Image Name")]
             private string ImageName { get; set; }
+
             private ImageFormats ImageFormat { get; set; }
+
             private double Height { get; set; }
+
             private double Width { get; set; }
 
     }
