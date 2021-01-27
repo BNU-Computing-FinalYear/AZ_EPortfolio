b8c56bdab86e0160a9f7c2b8d7a17d966cb3b0f3
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:41:49 +0000

    Added EvidenceTypes enumeration

---
+++ AZ_EPortfolio/Models/EvidenceTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public enum EvidenceTypes
+    {
+		Illustration,
+		Document,
+		Presentation,
+		Spreadsheet,
+		Audio,
+		Video,
+		Link,
+
+    }
+}
