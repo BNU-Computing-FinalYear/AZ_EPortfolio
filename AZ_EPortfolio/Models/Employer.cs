788a06a86e072fdcb27b51df65dbbd43bc847e84
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 13:37:41 +0000

    Added Employer class and attributes

---
+++ AZ_EPortfolio/Models/Employer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Models
+{
+    public class Employer : User
+    {
+        private int employerID;
+        private String companyName;
+        private String description;
+    }
+}
