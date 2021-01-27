10f0111de66e6951bf5f6f4ca4b50a71e1bdab26
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-27 14:41:41 +0000

    Added Annotations to TextEvidence class

--- AZ_EPortfolio/Models/TextEvidence.cs
+++ AZ_EPortfolio/Models/TextEvidence.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,15 @@ namespace AZ_EPortfolio.Models
 {
     public class TextEvidence : Evidence
     {
+        [Key]
         private int TextEvidenceID { get; set; }
+
+        [Required]
+        [StringLength(5000)]
+        [DisplayName("Description")]
+        [DataType(DataType.MultilineText)]
         private string Text { get; set; }
+
         private TextFormats TextFormat { get; set; }
 
     }
