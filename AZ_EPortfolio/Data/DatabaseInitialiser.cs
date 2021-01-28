fc9c58ebfa1a57b8de2ac91f910e58de0dca54bb
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-01-28 15:06:12 +0000

    Added DatabaseInitialiser and linked it to the program.cs

---
+++ AZ_EPortfolio/Data/DatabaseInitialiser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZ_EPortfolio.Data
+{
+    public class DatabaseInitialiser
+    {
+        public static void Initialise(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
