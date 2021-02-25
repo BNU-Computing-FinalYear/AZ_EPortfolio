333c5e24e83f8cfd9785fbc7c5cdfcb143821e26
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-25 23:32:17 +0000

    Scaffolded template's views and controller

---
+++ AZ_EPortfolio/TemplatesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AZ_EPortfolio.Data;
+using AZ_EPortfolio.Models;
+
+namespace AZ_EPortfolio
+{
+    public class TemplatesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TemplatesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Templates
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Templates.Include(t => t.Portfolio);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Templates/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var template = await _context.Templates
+                .Include(t => t.Portfolio)
+                .FirstOrDefaultAsync(m => m.TemplateId == id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            return View(template);
+        }
+
+        // GET: Templates/Create
+        public IActionResult Create()
+        {
+            ViewData["PortfolioId"] = new SelectList(_context.Portfolios, "PortfolioId", "Author");
+            return View();
+        }
+
+        // POST: Templates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TemplateId,PortfolioId,TemplateType,Summary,Picture,SkillTags,Resume,Education")] Template template)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(template);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PortfolioId"] = new SelectList(_context.Portfolios, "PortfolioId", "Author", template.PortfolioId);
+            return View(template);
+        }
+
+        // GET: Templates/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var template = await _context.Templates.FindAsync(id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+            ViewData["PortfolioId"] = new SelectList(_context.Portfolios, "PortfolioId", "Author", template.PortfolioId);
+            return View(template);
+        }
+
+        // POST: Templates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TemplateId,PortfolioId,TemplateType,Summary,Picture,SkillTags,Resume,Education")] Template template)
+        {
+            if (id != template.TemplateId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(template);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TemplateExists(template.TemplateId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PortfolioId"] = new SelectList(_context.Portfolios, "PortfolioId", "Author", template.PortfolioId);
+            return View(template);
+        }
+
+        // GET: Templates/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var template = await _context.Templates
+                .Include(t => t.Portfolio)
+                .FirstOrDefaultAsync(m => m.TemplateId == id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            return View(template);
+        }
+
+        // POST: Templates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var template = await _context.Templates.FindAsync(id);
+            _context.Templates.Remove(template);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TemplateExists(int id)
+        {
+            return _context.Templates.Any(e => e.TemplateId == id);
+        }
+    }
+}
