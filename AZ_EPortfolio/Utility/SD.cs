243bccb8567557aaf7ed7df41671685644e64b61
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-26 19:43:55 +0000

    added constant strings for different roles

--- AZ_EPortfolio/Utility/SD.cs
+++ AZ_EPortfolio/Utility/SD.cs
@@ -7,5 +7,11 @@ namespace AZ_EPortfolio.Utility
 {
     public static class SD
     {
+        public const string UndergraduateUser = "Undergraduate";
+        public const string PostgraduateUser = "Postgraduate";
+        public const string ApprenticeUser = "Apprentice";
+        public const string AdminUser = "Admin";
+        public const string EmployerUser = "Employer";
+        public const string DeveloperUser = "Developer";
     }
 }
