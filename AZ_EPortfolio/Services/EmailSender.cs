73df3060a03467267d5884f9f2383aa603d91000
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-27 07:57:42 +0000

    Extended EmailServer class to IEmailServer

--- AZ_EPortfolio/Services/EmailSender.cs
+++ AZ_EPortfolio/Services/EmailSender.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AZ_EPortfolio.Services
 {
-    public class EmailSender
+    public class EmailSender : IEmailSender
     {
     }
 }
