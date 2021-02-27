ccbb2a2fd0595c15efa1a27395b0110df2a3ad3b
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-27 08:03:20 +0000

    Added using Microsoft.AspNetCore.Identity.UI.Services

--- AZ_EPortfolio/Startup.cs
+++ AZ_EPortfolio/Startup.cs
@@ -1,9 +1,11 @@
 using AZ_EPortfolio.Data;
+using AZ_EPortfolio.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
