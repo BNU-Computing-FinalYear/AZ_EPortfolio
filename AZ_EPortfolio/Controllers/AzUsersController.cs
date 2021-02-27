df7d209eb5fd5cca8a0d8f8aabc2ce7f3a1e2dd3
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-27 10:13:41 +0000

    Moved Images folder into www.root

--- AZ_EPortfolio/Controllers/AzUsersController.cs
+++ AZ_EPortfolio/Controllers/AzUsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AZ_EPortfolio.Data;
 using AZ_EPortfolio.Models;
+using System.Security.Claims;
 
 namespace AZ_EPortfolio.Controllers
 {
@@ -14,19 +15,25 @@ namespace AZ_EPortfolio.Controllers
     public class AzUsersController : Controller
     {
         private readonly ApplicationDbContext _db;
-
-        private readonly ApplicationDbContext _context;
-
-        public AzUsersController(ApplicationDbContext context)
+        public AzUsersController(ApplicationDbContext db)
         {
-            _context = context;
+            _db = db;
         }
-
-        // GET: AzUsers
         public async Task<IActionResult> Index()
         {
-            return View(await _context.AzUser.ToListAsync());
+            //Fetching Id of user that is logged in
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //and passing all the application user except the loggin user view
+            return View(await _db.ApplicationUser.Where(u => u.Id != claim.Value).ToListAsync());
         }
+
+        //// GET: AzUsers
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _db.AzUser.ToListAsync());
+        //}
 
         // GET: AzUsers/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -36,7 +43,7 @@ namespace AZ_EPortfolio.Controllers
                 return NotFound();
             }
 
-            var azUser = await _context.AzUser
+            var azUser = await _db.AzUser
                 .FirstOrDefaultAsync(m => m.AzUserId == id);
             if (azUser == null)
             {
@@ -61,8 +68,8 @@ namespace AZ_EPortfolio.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(azUser);
-                await _context.SaveChangesAsync();
+                _db.Add(azUser);
+                await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(azUser);
@@ -76,7 +83,7 @@ namespace AZ_EPortfolio.Controllers
                 return NotFound();
             }
 
-            var azUser = await _context.AzUser.FindAsync(id);
+            var azUser = await _db.AzUser.FindAsync(id);
             if (azUser == null)
             {
                 return NotFound();
@@ -100,8 +107,8 @@ namespace AZ_EPortfolio.Controllers
             {
                 try
                 {
-                    _context.Update(azUser);
-                    await _context.SaveChangesAsync();
+                    _db.Update(azUser);
+                    await _db.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -127,7 +134,7 @@ namespace AZ_EPortfolio.Controllers
                 return NotFound();
             }
 
-            var azUser = await _context.AzUser
+            var azUser = await _db.AzUser
                 .FirstOrDefaultAsync(m => m.AzUserId == id);
             if (azUser == null)
             {
@@ -142,15 +149,15 @@ namespace AZ_EPortfolio.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var azUser = await _context.AzUser.FindAsync(id);
-            _context.AzUser.Remove(azUser);
-            await _context.SaveChangesAsync();
+            var azUser = await _db.AzUser.FindAsync(id);
+            _db.AzUser.Remove(azUser);
+            await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool AzUserExists(int id)
         {
-            return _context.AzUser.Any(e => e.AzUserId == id);
+            return _db.AzUser.Any(e => e.AzUserId == id);
         }
     }
 }
