b72373c78449479a9d2d053a0261f63e5daa4ba4
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-27 07:58:20 +0000

    Implemented SendEmailAsync interface

--- AZ_EPortfolio/Services/EmailSender.cs
+++ AZ_EPortfolio/Services/EmailSender.cs
@@ -8,5 +8,9 @@ namespace AZ_EPortfolio.Services
 {
     public class EmailSender : IEmailSender
     {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
