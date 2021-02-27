d0c231f885dac3dec71436782eaf1dc97d2419c9
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2021-02-27 08:22:27 +0000

    Denpendency injection added private readonly ApplicationDbContext _db

--- AZ_EPortfolio/Controllers/UserController.cs
+++ AZ_EPortfolio/Controllers/UserController.cs
@@ -9,6 +9,7 @@ namespace AZ_EPortfolio.Controllers
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly ApplicationDbContext _db;
         public IActionResult Index()
         {
             return View();
